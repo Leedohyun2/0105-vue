766dca202a12261cb8a371f974b5dd4a9f1b76cf
Leedohyun <ehqhddl123@naver.com>
2024-01-16 15:56:50 +0900

    0116정리 추가

---
+++ 0115(2)/06_router/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <!-- 앱바 테마를 primary(파란색)로 설정 -->
+    <v-app-bar app color="primary" dark>
+      <!-- 좌측에 메뉴 아이콘 생성 -->
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>멀티 페이지</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-slide-x-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-x-transition>
+    </v-main>
+
+    <v-footer color="secondary" fixed dark>
+      <div class="mx-auto">Create By VueJS</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
