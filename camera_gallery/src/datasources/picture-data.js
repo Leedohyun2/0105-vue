9e347907e736527d1eb0a18be4b00ec853decfa9
Leedohyun <ehqhddl123@naver.com>
2024-03-20 12:30:46 +0900

    Add files via upload

---
+++ camera_gallery/src/datasources/picture-data.js
@@ -0,0 +1,75 @@
+export default {
+  aPictures: [{
+    'id': 1,
+    'url': 'https://www.mancity.com/meta/media/oipfxjgl/bobb-winner.jpg?width=352&height=198&mode=crop',
+    'title': '꽃',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 2,
+    'url': 'https://www.mancity.com/meta/media/l0wl3ckm/wide-highlightsnew-a.jpg?width=352&height=198&mode=crop',
+    'title': '문',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 3,
+    'url': 'https://cdn.pixabay.com/photo/2024/01/03/13/01/trees-8485455_1280.jpg',
+    'title': '길',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 4,
+    'url': 'https://cdn.pixabay.com/photo/2024/01/07/16/27/reed-8493547_1280.jpg',
+    'title': '갈대',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 5,
+    'url': 'https://web-assets.mancity.com/cdn-cgi/image/width=683,format=auto/dist/images/cityzens-bg-1920x1080.png',
+    'title': '거리',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 6,
+    'url': 'https://www.mancity.com/meta/media/vrmnrfpx/pep-htown-presser-wide-2.jpg?width=169&height=95&mode=crop',
+    'title': '악보',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 7,
+    'url': 'https://www.mancity.com/meta/media/pjdaz4rf/4-0-foden.jpg?width=169&height=95&mode=crop',
+    'title': '카메라',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 8,
+    'url': 'https://cdn.pixabay.com/photo/2023/12/08/10/25/church-8437403_1280.jpg',
+    'title': '호수',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 9,
+    'url': 'https://cdn.pixabay.com/photo/2023/11/27/09/49/lighthouse-8415024_1280.jpg',
+    'title': '등대',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 10,
+    'url': 'https://cdn.pixabay.com/photo/2023/12/16/10/51/oak-leaf-8452141_1280.jpg',
+    'title': '낙엽',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 11,
+    'url': 'https://cdn.pixabay.com/photo/2024/01/03/17/50/sunset-8485910_1280.jpg',
+    'title': '석양',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  },
+  {
+    'id': 12,
+    'url': 'https://cdn.pixabay.com/photo/2016/12/03/15/44/fireworks-1880045_1280.jpg',
+    'title': '불꽃',
+    'info': '이미지 출처 : 픽사베이(https://pixabay.com)'
+  }
+  ]
+}
